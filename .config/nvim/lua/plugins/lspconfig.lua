cebf73f5c58196bf192e71396a73b9110f9579ed
heyitscarl <carl.nw.neben@gmail.com>
2025-05-29 20:56:18 +0200

    feat: added diagnostic menu keybind

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -17,6 +17,7 @@ return {
       { "K", vim.lsp.buf.hover, desc = "View hover menu" },
       { "<Leader>gd", vim.lsp.buf.definition, desc = "Go to definition" },
       { "<Leader>ca", vim.lsp.buf.code_action, desc = "View code actions" },
+      { "<Leader>d", vim.diagnostic.open_float, desc = "View diagnostics" }
     },
     lazy = false
 }
